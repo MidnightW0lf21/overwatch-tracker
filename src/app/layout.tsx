f5a11ed80372812b81fe3321d8c95e7b2fc8c9b6
Luke „Midnight“ <Mr.hahaah@gmail.com>
2025-08-04 16:54:18 +0000

    Please add Vercel Web Analytcis to this app: import { Analytics } from "

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import type {Metadata} from 'next';
 import { Inter } from 'next/font/google'; 
 import './globals.css';
 import { Toaster } from "@/components/ui/toaster"; 
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({
   subsets: ['latin'],
@@ -29,8 +30,8 @@ export default function RootLayout({
       <body className="antialiased font-sans"> 
         {children}
         <Toaster /> 
+        <Analytics />
       </body>
     </html>
   );
 }
-
